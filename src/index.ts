1a228ea1af4e13ed78c11866a55682d6f46836a8
Ariful Alam <swazan.arif@gmail.com>
2022-06-13 20:56:21 +0600

    Add option to provide model file path

--- src/index.ts
+++ src/index.ts
@@ -68,8 +68,12 @@ export class NSFWJS {
       typeof modelPathBaseOrIOHandler === "string" &&
       !modelPathBaseOrIOHandler.startsWith("indexeddb://") &&
       !modelPathBaseOrIOHandler.startsWith("localstorage://")
-    ) {  
-      this.pathOrIOHandler = `${modelPathBaseOrIOHandler}model.json`;
+    ) {
+      if (modelPathBaseOrIOHandler.includes("model.json")) {
+        this.pathOrIOHandler = modelPathBaseOrIOHandler;
+      } else {
+        this.pathOrIOHandler = `${modelPathBaseOrIOHandler}model.json`;
+      }
     } else {
       this.pathOrIOHandler = modelPathBaseOrIOHandler;
     }
