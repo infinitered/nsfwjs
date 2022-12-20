7694b098fda0c5b07ada2256610cb93b4ac4dcc4
Dan Edwards <cdaniel.edwards@gmail.com>
2022-12-20 14:27:52 -0500

    fix: update overrides

--- example/nsfw_demo/config-overrides.js
+++ example/nsfw_demo/config-overrides.js
@@ -3,13 +3,7 @@ const webpack = require('webpack');
 module.exports = function override(config) { 
 		const fallback = config.resolve.fallback || {}; 
 		Object.assign(fallback, { 
-    	// "crypto": require.resolve("crypto-browserify"), 
         "stream": require.resolve("stream-browserify"), 
-        // "assert": require.resolve("assert"), 
-        // "http": require.resolve("stream-http"), 
-        // "https": require.resolve("https-browserify"), 
-        // "os": require.resolve("os-browserify"), 
-        // "url": require.resolve("url"),
         "path": require.resolve("path-browserify"),
         "zlib": require.resolve("browserify-zlib"),
 
