6a250901062191ced003190a954ee8b9f54318f4
Mazen Chami <mazenchami@gmail.com>
2024-02-12 16:33:14 -0500

    testing blocking specific country

---
+++ examples/nsfw_demo/netlify/edge-functions/country-block.tsx
@@ -0,0 +1,29 @@
+import type { Context, Config } from "@netlify/edge-functions";
+
+export default async (request: Request, context: Context) => {
+  console.log({ request, context });
+  const BLOCKED_COUNTRY_CODE = "US"; // "RU";
+  const countryCode = context.geo?.country?.code ?? "US";
+  const countryName = context.geo?.country?.name ?? "United States of America";
+
+  if (countryCode === BLOCKED_COUNTRY_CODE) {
+    return new Response(
+      `We're sorry, you can't access our content from ${countryName}!`,
+      {
+        headers: { "content-type": "text/html" },
+        status: 451,
+      }
+    );
+  }
+
+  return new Response(
+    `Hello there! You can freely access our content from ${countryName}!`,
+    {
+      headers: { "content-type": "text/html" },
+    }
+  );
+};
+
+export const config: Config = {
+  path: "/*",
+};
