72b8f800cea48f920acf8e4e78b7751a268fb320
haZya <63403456+haZya@users.noreply.github.com>
2026-02-22 01:09:47 +0530

    fix(demo): header layout shift

--- examples/nsfw_demo/src/components/Header.tsx
+++ examples/nsfw_demo/src/components/Header.tsx
@@ -4,18 +4,23 @@ import tfjsLogo from "../assets/tfjs-logo.jpg";
 const Header = () => (
   <header className="bg-background-main px-4">
     <div className="container mx-auto flex flex-wrap items-center justify-center gap-x-4 sm:flex-nowrap">
-      <img id="logo" className="max-h-20 min-w-36 sm:max-h-36" src={nsfwjsLogo} alt="Logo" />
+      <img
+        id="logo"
+        className="h-20 w-50 object-contain sm:h-36 sm:w-64 md:w-90"
+        src={nsfwjsLogo}
+        alt="Logo"
+      />
       <h1 className="py-3 text-xl font-bold italic max-sm:order-last max-sm:text-center sm:text-2xl">
         Client-side indecent content checking
       </h1>
-      <div className="text-primary-main flex flex-col items-center gap-2 font-bold sm:ml-auto">
+      <div className="text-primary-main flex flex-col items-center font-bold sm:ml-auto">
         <p className="from-primary-dark to-primary-light bg-linear-to-r bg-clip-text text-base text-transparent sm:text-lg">
           Powered&nbsp;by
         </p>
         <a href="https://js.tensorflow.org/" target="_blank" rel="noreferrer">
           <img
             id="tflogo"
-            className="max-h-12 min-w-20 sm:max-h-20"
+            className="h-12 w-20 object-contain sm:h-20 sm:w-32"
             src={tfjsLogo}
             alt="TensorflowJS Logo"
           />
